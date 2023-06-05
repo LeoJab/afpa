f7edd41b1e7890cb924cec6d5789c1bade0d3258
LeoJab <leojabelin@laposte.net>
2023-06-05 17:01:11 +0200

    ms dev appli

---
+++ monProjet/public/assets/js/main.js
@@ -0,0 +1,15 @@
+﻿const menu_nav_uti = document.querySelector('.menu_nav_uti');
+console.log(menu_nav_uti);
+
+const icon_user = document.querySelector('.icon_user');
+console.log(icon_user);
+
+icon_user.addEventListener("click", function(){
+    if(menu_nav_uti.classList.contains('none')) {
+        menu_nav_uti.classList.toggle('none');
+        menu_nav_uti.classList.toggle('block');
+    } else {
+        menu_nav_uti.classList.toggle('block');
+        menu_nav_uti.classList.toggle('none');
+    }
+})
